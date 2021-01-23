7527f3284acc91c34ce04e202a145a8c72a771e2
wty262690 <71436111+wty262690@users.noreply.github.com>
2021-01-23 14:46:01 +0800

    photo_replace

--- src/views/example4.vue
+++ src/views/example4.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="inturd">
-    <p>《春節自救指南》- 上海彩虹室內合唱團</p>
-    <p>《春節自救指南》</p>
-    <p>(What I Do Is For Your Own Good)</p>
+    <img class="left" src="https://raw.githubusercontent.com/wty262690/vue-p5-finalhomework-git/main/src/photo/left.png" style="float:left; margin: 5px;" />
+    <p class="songname">春節自救指南</p>
+    <p>What I Do Is For Your Own Good</p>
+    <p>上海彩虹室內合唱團</p>
+    
     <p>
         演唱：上海彩虹室內合唱團
         詞曲：金承志
@@ -10,20 +12,34 @@
         鋼伴：白苑彤
         混音：莫家偉
     </p>
-    <p>
-那一天，人類終於回想起了，曾經春節回家的恐怖，
-還有被各路親戚超負荷關心所支配的那份屈辱
-
-《春節自救指南》是由上海彩虹室內合唱團自主研發的神奇藥丸，專治父母逼婚、親戚圍堵、熊孩爆炸、隔壁老王等春節頑疾。
-如您曾接受過全家靈魂深處的拷問、曾被親戚的超負荷關愛電磁炮所擊中，請務必服此良藥。
-    </p>
     </div>
-    <img src="https://raw.githubusercontent.com/wty262690/vue-p5-finalhomework-git/main/src/photo/test2.png">
+    
+    <img class="right" src="https://raw.githubusercontent.com/wty262690/vue-p5-finalhomework-git/main/src/photo/right.png" style="float:right; margin: 5px;" />
+    <p class="inturd">
+        那一天，人類終於回想起了，曾經春節回家的恐怖，
+        還有被各路親戚超負荷關心所支配的那份屈辱
+
+        《春節自救指南》是由上海彩虹室內合唱團自主研發的神奇藥丸，專治父母逼婚、親戚圍堵、熊孩爆炸、隔壁老王等春節頑疾。
+        如您曾接受過全家靈魂深處的拷問、曾被親戚的超負荷關愛電磁炮所擊中，請務必服此良藥。
+    </p>
 </template>
+
 <style>
 .inturd{
     position:relative;
     z-index: 2;
+    color:white;
 }
-
+img{
+    position:relative;
+    z-index: 2;
+    top:-20px;
+}
+.songname{
+    color:rgb(213, 228, 148);
+    padding-top:15vh;
+    font-size:60px;
+    font-family:"cat";
+    letter-spacing: 10px;
+}
 </style>
